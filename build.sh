37eb965221acf80d303cef39b6d113bc3c6f79d4
RhidAdmin <rhidian_nelson@rhidian-jenkins-instance.europe-west4-a.c.lbg-210222.internal>
2022-03-04 14:08:39 +0000

    sprint2

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /home/rhidian_nelson/LBG-API
+docker build -t myapp:latest .
--- start.js
+++ start.js
@@ -1,3 +1,4 @@
+// New Change for Sprint 2
 // import app from app.js
 const app = require('./app').app;
 
